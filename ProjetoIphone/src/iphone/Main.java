3c42073e60f33eaceb4a9db52142d57f384cc252
Osleno Rocha <osleno24silva@gmail.com>
2025-04-27 00:47:54 -0300

    Update Main.java

--- ProjetoIphone/src/iphone/Main.java
+++ ProjetoIphone/src/iphone/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
         Iphone iphone = new Iphone();
         
-        iphone.ligar("(11)91659-8329");
+        iphone.ligar("(11)91458-5697");
         iphone.atender();
         iphone.iniciarCorreioVoz();
         iphone.selecionarMusica("Apenas mais uma dose");
         iphone.tocar(); 
         iphone.pausar();    
-        iphone.exibirPagina();
+        iphone.exibirPagina("WWW.teste.com.br");
         iphone.adicionarNovaAba();    
         iphone.atualizarPagina();
     }
